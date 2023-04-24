bac6e3e5f6ee847e8ba19fdbaaefec257ec3887c
ginoynacho <127965100+ginoynacho@users.noreply.github.com>
2023-04-24 14:34:30 -0300

    Create plancha 2 .hs

---
+++ plancha 2 .hs
@@ -0,0 +1,17 @@
+--yo lo tengo a olmedo, yo lo tengo al che y vos q carajo tenes
+--mi nombre es el da la ciudad
+--el tuyo no se donde es
+--un equipo cagon que abandona, todos putos q rompen carnet
+--son tan putos q hace 100 años llevan el nombre de un ingles
+--carnavaaal carnavaaal
+--el carnaval es el pueblo y el pueblo es hincha de central
+--carnavaaal carnavaaal
+--el carnaval es el pueblo y el pueblo es hincha de central
+--vamo la kd
+--vamo la kd
+--vamo la kd
+--vamo la kd
+--------------------------------------------------------------------------------------------------------------------------------
+factorial::Float->Float
+factorial 0 = 1
+factorial n = n * factorial(n-1)
